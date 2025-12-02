40550ec4114cf509d98946a1fb3c3fc7d7ea111a
Ankush <kush.agv@gmail.com>
2025-12-02 22:25:21 +0100

    style changes

--- frontend/src/pages/CreateMatch.tsx
+++ frontend/src/pages/CreateMatch.tsx
@@ -29,34 +29,47 @@ const CreateMatch = function () {
 
   return (
     <>
-      <form>
-        <div>
-          <label> Total Points: </label>
-          <input type="number" placeholder="ex: 21" min={0} max={30}></input>
-        </div>
-        <div>
-          <label>Add Players: </label>
-
-          {inputs.map((value, index) => (
-            <div key={index}>
-            <input
-              type="text"
-              value={value}
-              onChange={(e) => handleChange(index, e.target.value)}
-              placeholder='John Doe'
-            />
-            </div>
-          ))}
-          <div>
-            <button type="button" onClick={addPlayer}>Add a player</button>
-            <button type="button" onClick={deletePlayer}>Delete a player</button>
+      <form className="form-control">
+        <div className="row g-3 align-items-center">
+          <div className="col-auto">
+            <label className="col-form-label"> Total Points: </label>
+          </div>
+          <div className="col-auto">
+            <input type="number" placeholder="ex: 21" min={0} max={30} className="form-control"></input>
           </div>
         </div>
-        <div>
-          <label> Number of Courts: </label>
-          <input type="number" placeholder="1" min={1} max={10}></input>
+        <div className="row g-3 align-items-center">
+          <div className="col-auto">
+            <label className="col-form-label">Add Players: </label>
+          </div>
+          <div className="col-auto">
+            {inputs.map((value, index) => (
+              <div key={index}>
+                <input
+                  type="text"
+                  value={value}
+                  onChange={(e) => handleChange(index, e.target.value)}
+                  placeholder='John Doe'
+                  className="form-control"
+                />
+              </div>
+            ))}
+
+            <div>
+              <button type="button" onClick={addPlayer} className="btn btn-success">Add a player</button>
+              <button type="button" onClick={deletePlayer} className="btn btn-danger">Delete a player</button>
+            </div>
+          </div>
         </div>
-        <button type='button' onClick={startMatch}>Start Match</button>
+        <div className="row g-3 align-items-center">
+          <div className="col-auto">
+            <label className="col-form-label"> No. of Courts: </label>
+          </div>
+          <div className="col-auto">
+            <input type="number" placeholder="1" min={1} max={10} className="form-control"></input>
+          </div>
+        </div>
+        <button type='button' onClick={startMatch} className="btn btn-primary">Start Match</button>
       </form>
     </>
   )
